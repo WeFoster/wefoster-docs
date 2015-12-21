e6fae469224c6277faec3c33ee7bbb5e0db7156a
Bowe Frankema <bowe@cfcommunity.net>
2015-12-21 14:09:18 +0100

    Improve Docs theme

--- child-lib/acf.php
+++ child-lib/acf.php
@@ -6,11 +6,11 @@
 function wfd_recommened_reading() { ?>
         <div class="recommended-reading">
 
-          <h3>Further Reading</h3>
+          <h3>Additional Resources</h3>
 
               <?php
               // get field value
-               $posts = get_field('further_reading');
+               $posts = get_field('additional_reading');
 
                if( $posts ):
                    // switch to multisite
@@ -29,4 +29,28 @@ function wfd_recommened_reading() { ?>
 
 <?
 }
-add_action( 'wpkb_after_article_content','wfd_recommened_reading', 1 );
+add_action( 'wpkb_after_article_content','wfd_recommened_reading', 10 );
+
+function wfd_further_reading() { ?>
+        <div class="further-reading">
+
+          <h3>What's next?</h3>
+
+                <?php
+                // get field value
+                 $posts = get_field('further_reading');
+
+                 if( $posts ): ?>
+                     <ul>
+                         <?php foreach ($posts as $p): ?>
+                             <li>
+                                <a href="<?php echo get_permalink( $p->ID ); ?>"><?php echo get_the_title( $p->ID ); ?></a>
+                             </li>
+                         <?php endforeach; ?>
+                     </ul>
+                 <?php endif; ?>
+
+        </div>
+<?
+}
+add_action( 'wpkb_after_article_content','wfd_further_reading', -999 );
